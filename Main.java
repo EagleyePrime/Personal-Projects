f074c54b68788dfbc6966358083f413bb9c49552
EagleyePrime <kuangdemetrius@gmail.com>
2024-07-28 18:54:38 -0700

    random tips for website

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("yabba dabba do");
         WebSite w = new WebSite();
-        System.out.println(w.randMsg());
     }
 }
--- WebSite.java
+++ WebSite.java
@@ -1,30 +1,36 @@
 public class WebSite {
-  
-  //Realistically, we're probably going to have to create subclasses. Not now, but may
-  private float gpa;                                                                   
-  private String[] favorites;                                                          
-  private float income;                                                                
-  private int age;                                                                     
-                                                                                      
-  private boolean isPoor;                                                              
-  private boolean isAtCasino;                                                          
-                                                                                      
-  private String[] tips = new String[1];                                                               
-                                                                                      
-  public WebSite() {                                                                   
-      if (isPoor) {                                                                   
-          isAtCasino = true;                                                          
-      }                                                                               
-      else {                                                                          
-          isAtCasino = true;                                                          
-      }          
-      tips[0] = "Get rich. Gamble.";                                                                 
-  }                                                                                   
-                                                                                      
-  //Precondition: tips.length > 0                                                     
-  public String randMsg() {                                                            
-      int rIndex = (int) (Math.random() * tips.length);   //Random index.             
-      return tips[rIndex];                                                            
-  } 
+
+    //Realistically, we're probably going to have to create subclasses. Not now, but maybe in the near future.
+    private static String[] tips;   //Random fun tips or messages given to the user.
+
+    public WebSite() {
+        implementRandTips();
+        System.out.println("Tip of the day: " + randMsg());
+    }
+
+    //Precondition: tips.length > 0
+    private static String randMsg() {
+        int rIndex = (int) (Math.random() * tips.length);   //Random index.
+        return tips[rIndex];
+    }
+
+    private static void implementRandTips() {
+        tips = new String[5];
+        tips[0] = "You can choose mundane things you’ve done to put in your college application; if you spice it up with the right words, it’ll work.";
+        tips[1] = "Keep digging, you’ll reach gold eventually.";
+        tips[2] = "If it works, it works.";
+        tips[3] = "Some call it an addiction, I call it dedication.";
+        tips[4] = "Do not take this website as gospel.";
+    }
+    
+
+
+
+
+
+
+
+
+
 
 }
