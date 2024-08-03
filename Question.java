1ee829df1dadbb83101a8d6634efdd04edf6c989
EagleyePrime <kuangdemetrius@gmail.com>
2024-08-02 20:55:50 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -2,12 +2,13 @@ public class Question {
     private String questionText;
     private String[] options;
     private String[] categories;
-    
+
     //For MCQ-style questions.
     public Question(String questionText, String[] options, String[] categories) {
         this.questionText = questionText;
         this.options = options;
         this.categories = categories;
+
     }
 
     public String getQuestionText() {
