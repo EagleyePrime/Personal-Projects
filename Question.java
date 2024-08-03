fb4833ea6de1f33a4417ad1ff7e2b061875882b2
EagleyePrime <kuangdemetrius@gmail.com>
2024-08-02 20:23:19 -0700

    removed unnecessary stuff

--- Question.java
+++ Question.java
@@ -2,23 +2,12 @@ public class Question {
     private String questionText;
     private String[] options;
     private String[] categories;
-    private float numericalAnswer;
-    private boolean isNumerical;
-    private boolean isConfirmed; //True means the user confirmed their answer.
-
+    
     //For MCQ-style questions.
     public Question(String questionText, String[] options, String[] categories) {
         this.questionText = questionText;
         this.options = options;
         this.categories = categories;
-        isNumerical = false;
-    }
-
-    //For questions that ask for a numerical input.
-    public Question(String questionText, float num) {
-        this.questionText = questionText;
-        numericalAnswer = num;
-        isNumerical = true;
     }
 
     public String getQuestionText() {
@@ -33,15 +22,4 @@ public class Question {
         return categories;
     }
 
-    public float getNumber() {
-        return numericalAnswer;
-    }
-
-    public boolean isCustomAnswer(){
-        return isNumerical;
-    }
-
-    public boolean checkForConfirmation() {
-        return isConfirmed;
-    }
 }
